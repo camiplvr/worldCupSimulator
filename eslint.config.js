697abfef99f37b8d1d98154b840ea78fe078a26d
Camila Ribeiro <cami.plvr@gmail.com>
2026-03-28 18:02:59 -0300

    chore: configure eslint with react and typescript and setup vitest with jsdom

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-])
+
+  prettier,
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './index.css'
+import "./index.css";
 
- function App() {
+function App() {
   return (
-    <div className="bg-red-500 text-white p-10 text-2xl">
-      HELLO WORLD 🔥
-    </div>
+    <div className="bg-red-500 text-white p-10 text-2xl">HELLO WORLD 🔥</div>
   );
 }
-export default App ;
-
+export default App;
